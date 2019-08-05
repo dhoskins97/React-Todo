d04474d15e4086f1b10993013e7a70cdbfe4a81c
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-08-05 18:58:33 -0400

    added mapping functionality

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <>
+            {this.props.listStorage.map((listItem) => {
+                return <div>{listItem.title}</div>
+            })}
+            </>
+        )
+    }
+}
+
+export default TodoList;
